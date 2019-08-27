2a23756182c4eccc294e55b9e7d0f44c8b53ef00
jyodying <xpanderz@yahoo.com>
2019-08-26 21:18:32 -0400

    Angular 7 HTTP Client

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,10 +9,15 @@ import { DataService } from '../data.service';
 export class HomeComponent implements OnInit {
 
   h1Style: boolean = false;
+  users: Object;
 
   constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getUsers().subscribe(data => {
+      this.users = data
+      console.log(this.users)
+    })
   }
 
   firstClick() {
